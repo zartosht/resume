b66ed97dc8cf6091335a9dab891eb5be29b3a82a
Zartosht Sepideman <Xashayarsha@gmail.com>
2020-07-04 22:03:59 +0430

    first commit

---
+++ data/data.js
@@ -0,0 +1,191 @@
+
+// Change your profile data here
+const profile = {
+  // picture: "/img/me.jpg",
+  name: 'Mohamad Karbalaei (Zartosht Sepideman)',
+  description:
+    "I'm a Backend developer working with PHP, NodeJS, Python and SQL, NoSQL Databases based on project needs.",
+  bio:
+    `I've been programming for 7 years. I'm a Back-end  developer with experience creating, maintaining and implementing everything from databases to web pages.
+    I nerd out when learning new programming languages functionality feels giddy when finding /destroying bugs.
+    I like to face new challenges in every field in my career and I'm not afraid of them.
+    `,
+  email: 'hi@zartosht.dev',
+  phone: '+98 912 77 18 19 4',
+  location: 'Tehran, Iran',
+  birth: '1989-07-14', // YYYY-MM-DD format
+  skills: [
+    'PHP',
+    'Laravel',
+    'Code Igniter',
+    'Phalcon',
+    'Wordpress',
+    'Node.Js',
+    'Express Js',
+    'Nest',
+    'Adonis Js',
+    'python',
+    'Django',
+    'Hyperledger Fabric',
+    'Restful API',
+    'MicroService',
+    'Web Socket',
+    'Docker',
+    'CI/CD',
+    'Linux',
+    'Mysql',
+    'PostgreSQL',
+    'MongoDB',
+    'Redis',
+    'Git'
+  ],
+  social: {
+    linkedin: 'https://www.linkedin.com/in/zartoshtsepideman',
+    twitter: 'https://twitter.com/zartoshtsepid',
+    github: 'https://github.com/zartosht',
+    stackoverflow: 'https://stackoverflow.com/users/3632888/zartosht-sepideman',
+    blog: 'https://virgool.io/@zartosht'
+  },
+  experience: [
+    {
+      title: 'Full Stack Developer',
+      company: 'Shukamo',
+      url: 'https://Shukamo.com',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Networking.<br/>
+        - Wordpress theme development.<br/>
+        - Wordpress plugin development.<br/>
+        `,
+      from: 'Jan 2013',
+      to: 'Jan 2014',
+      stacks: 'PHP, Wordpress, Mysql'
+    },
+    {
+      title: 'Web Developer ',
+      company: 'Benis Rousta',
+      url: 'https://Shukamo.com',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Wordpress theme development. <br/>
+        - Wordpress plugin development. <br/>
+        - Javascript development for knightlab. <br/>
+        `,
+      from: 'Mar 2014',
+      to: 'Dec 2015',
+      stacks: 'PHP, Wordpress, Mysql, Javascript'
+    },
+    {
+      title: 'IT Manager and Full-stack Developer ',
+      company: 'TAV Company',
+      url: 'https://TAVCompany.com',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Development of wordpress plugins <br/>
+        - Development of wordpress themes <br/>
+        - Development of PHP modules <br/>
+        `,
+      from: 'Oct 2014',
+      to: 'Jun 2016',
+      stacks: 'PHP, Wordpress, Mysql, Javascript, CSS'
+    },
+    {
+      title: 'Fullstack developer',
+      company: 'Fidibo',
+      url: 'https://Fidibo.com',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Build and develop Desktop application of Fidibo <br/>
+        - Maintenace of website with Phalcon<br/>
+        `,
+      from: 'Nov 2016',
+      to: 'Mar 2017',
+      stacks: 'C#, .Net, Php, Phalcon'
+
+    },
+    {
+      title: 'Backend Developer',
+      company: 'MediaHamrah',
+      url: 'https://MediaHamrah.com',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Build and develop PHP microservices for using with VAS <br/>
+        - Develop REST APIs for interacting with MCI and MTN <br/>
+        - Develop backend of <a href="https://play.google.com/store/apps/details?id=com.mobilemediacomm.wallpapers">Everpics</a> (android app) <br/>
+        - Develop lottery services for automation of charging services for Hamrah Aval <br/>
+        `,
+      from: 'Jun 2018',
+      to: 'Jan 2019',
+      stacks: 'Php, Laravel, Symfony, Neo4j, Mongodb, Mysql, NodeJS, Elastic Search, REST API'
+    },
+    {
+      title: 'Lead Backend Developer ',
+      company: 'Dana Noyan Shahre raaz',
+      url: 'https://noyanpay.net',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Maintenance Noyanpay.net website  <br/>
+        - Develop CodeIgniter modules <br/>
+        - Develop PHP and wordpress plugins for envato  <br/>
+        - Manage developer team  <br/>
+        - Develop mobile application with ReactNative  <br/>
+        - Develop TV app with ReactNative <br/>
+        `,
+      from: 'Jan 2019',
+      to: 'Jan 2020',
+      stacks: 'PHP, CodeIgniter, React, ReactNative, Laravel, OctoberCMS, Wordpress, Mysql'
+
+    },
+    {
+      title: 'Lead Backend Developer',
+      company: 'Ariatak',
+      url: 'http://areatak.com',
+      location: 'Tehran, Iran',
+      description:
+        `
+        - Develop PHP microservices for Maskan bank project <br/>
+        - Works on blockchain based service for iran banking network <br/>
+        - Collaborate with the development and implementation of the KYC (Know Your Customer) system in blockchain platform With HyperLedger<br/>
+        - Implement the system using microservices architecture <br/>
+        - Developing project for Iranian centeral Bank in Khadamat Company <br/>
+        - Developing project for Maskan bank utilizing blockchain and NodeJS <br/>
+        `,
+      from: 'Jan 2020',
+      to: 'Present',
+      stacks: 'PHP, Laravel, NodeJs, ExpressJs, KoaJs, HyperLedger Fabric, MongoDB, Postgres, Redis, Socket Programming, Git, CI/CD, Docker, Adonis, Nest, REST API, Soap'
+    },
+  ],
+  education: [
+    {
+      school: 'Tehran Jonoub',
+      degree: 'Engineering',
+      fieldOfStudy: 'Civil Engineering',
+      location: 'Tehran, Iran',
+      from: '2007',
+      to: '2011'
+    }
+  ],
+  quote: 'It\'s better when everything goes according to plan!.'
+}
+
+const page = {
+  language: 'english',
+  // ^ languages suported:
+  //  - english (default)
+  //  - spanish
+  //  - italian
+  //  - catalan
+  //  - french
+  theme: 'nightsky'
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+}
