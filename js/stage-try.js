93b44bfe7acba4a5c1024ade42387799050c44f4
Zartosht Sepideman <xashayarsha@gmail.com>
2022-01-24 23:54:04 +0300

    test shop variable

--- js/stage-try.js
+++ js/stage-try.js
@@ -1,6 +1,6 @@
 console.log('run1')
 console.log(stage_cart_total_price)
-document.addEventListener('readystatechange', function() {
+document.addEventListener('DOMContentLoaded', function() {
     console.log('run')
     console.log(window.location.href);
     console.dir(shop)
